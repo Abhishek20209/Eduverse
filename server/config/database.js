548ce3abda9600ae930cfd788827cbff0c4a3d68
Abhishek <abhishek.hit.cse25@heritageit.edu.in>
2025-04-24 15:42:30 +0530

    Fix database connection issue

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,29 @@
-const mongoose=require("mongoose");
+// const mongoose=require("mongoose");
+// require("dotenv").config();
+
+// exports.connect=()=>{
+//     mongoose.connect(process.env.MONGODB_URL,{
+//         // useNewUrlParser:true,
+//         // useUnifiedTopology:true
+//     })
+//     .then(()=>{console.log("db connection successfull")})
+//     .catch((error)=>{
+//         console.log("issue in db connection");
+//         console.error(error); 
+//         process.exit(1);
+//     });
+// }
+
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-exports.connect=()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    })
-    .then(()=>{console.log("db connection successfull")})
-    .catch((error)=>{
-        console.log("issue in db connection");
-        console.error(error); 
-        process.exit(1);
-    });
-}
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL)
+        .then(() => {
+            console.log("DB connection successful");
+        })
+        .catch((error) => {
+            console.error("Issue in DB connection:", error);
+            process.exit(1);
+        });
+};
